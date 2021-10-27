6da7b44571ff524d6be525f7fe8f1f17f2bdf7ca
Yan Machado <yan.leoncio@gmail.com>
2021-10-27 10:17:57 -0300

    up

--- webclient/catalogo_filmes/script.js
+++ webclient/catalogo_filmes/script.js
@@ -7,10 +7,7 @@ http.get('https://rafaelescalfoni.github.io/desenv_web/filmes.json', function(st
         let aux = '';
         let ages = [];
 
-        const star = document.createElement("img");
-        star.setAttribute("src","/img/star.svg");
-        const starHalf = document.createElement("img");
-        starHalf.setAttribute("src","/img/starHalf.svg");
+        
 
         response.forEach(data => {
             const showRating = document.createElement("div");
@@ -33,18 +30,7 @@ http.get('https://rafaelescalfoni.github.io/desenv_web/filmes.json', function(st
                     generosAux += li.outerHTML;
                 });
 
-                data.opinioes.forEach((opinion, k) => {
-                    const starDiv = document.createElement("div");
-                    starDiv.className = 'rating'
-                    const span = document.createElement("span");
-                    span.appendChild(document.createTextNode(opinion.descricao));
-                    for(let i=0;i<opinion.rating;i++){
-                        starDiv.appendChild(star.cloneNode(true)); 
-                    }
-                    media = (media + opinion.rating)/(k+1);
-                    span.appendChild(starDiv);
-                    opinioesAux += span.outerHTML;
-                });
+                
 
                 data.titulosSemelhantes.forEach((similar, k) => {
                     response.forEach(test => {
@@ -56,14 +42,7 @@ http.get('https://rafaelescalfoni.github.io/desenv_web/filmes.json', function(st
                     });
                 });
 
-                for(let i=0;i<media;i++){
-                if(media % 1 !== 0 && i+1 >= media){
-                    showRating.appendChild(starHalf.cloneNode(true));
-                    break;
-                }
-                showRating.className = 'showRating';
-                showRating.appendChild(star.cloneNode(true)); 
-                }
+                
             } eachForLoops();
 
             aux += `<div class="card">
